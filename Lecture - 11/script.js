5a806e0a214b33715460b7ba6ffd0f84a0dd5638
Yogesh Kumar <kshanu385@gmail.com>
2026-07-02 21:07:18 +0530

    Lecture - 11 completed - Introduction to Javascript

---
+++ Lecture - 11/script.js
@@ -0,0 +1,39 @@
+// console.log(`Hello World!!!`);
+
+var a = 25; //number
+var b = "Javascript"; // string
+var c = false; // boolean
+
+// console.log(typeof a);
+// console.log(b);
+// console.log(c);
+
+var x; // x = undefined ;\
+
+var n;
+
+// console.log(x);
+// console.log(typeof x);
+
+x = null;
+
+// console.log(x);
+// console.log(typeof x);
+
+x = 23;
+
+var z = 1234567890987654321n; // bigint
+
+// console.log(z);
+
+var a1 = Symbol("yogesh");
+var a2 = Symbol("yogesh");
+
+// var myCar;
+
+var g1 = Symbol.for("myCar");
+var g2 = Symbol.for("myCar");
+
+// console.log(x);
+
+console.log(m);
